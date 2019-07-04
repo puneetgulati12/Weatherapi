a79a89ed3eaa46907b181f539dd1f840d8c8ab47
puneetgulati12 <puneetgulati97@gmail.com>
2019-07-04 17:58:17 +0530

    menu added

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.weather;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,27 +11,24 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
-import android.support.design.widget.TabLayout;
-
-import android.support.v4.view.ViewPager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -55,8 +53,9 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
 
 
     //This is our viewPager
+private ViewPager viewPager;
 
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,207 +67,242 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         }
 
         googleApiClient = new GoogleApiClient.Builder(this, this, this).addApi(LocationServices.API).build();
-    }
-    //Adding toolbar to the activity
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-    @Override
-    public void setSupportActionBar(@Nullable androidx.appcompat.widget.Toolbar toolbar) {
-        super.setSupportActionBar(toolbar);
-    }
-    //Initializing the tablayout
-   TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+//    //Adding toolbar to the activity
+//    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
 
 
-    //Adding the tabs using addTab() method
+        //Initializing the tablayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        //Initializing viewPager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+
+
+        //Adding the tabs using addTab() method
         tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-    //Initializing viewPager
-    ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
 
-    //Creating our pager adapter
-    Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
+        //Creating our pager adapter
 
-    //Adding adapter to pager
+
+        pager adapter = new pager(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
         viewPager.setAdapter(adapter);
 
-    //Adding onTabSelectedListener to swipe views
-        tabLayout.setOnTabSelectedListener(this);
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+
+            case R.id.setting:
+                Intent intent = new Intent(Settings.ACTION_SETTINGS);
+                startActivity(intent);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                return true;
+
+            case R.id.share:
+                Intent intent1 = new Intent(Intent.ACTION_SEND);
+                intent1.setType("plain/text");
+                intent1.putExtra(Intent.EXTRA_TEXT , "something" );
+                intent1.putExtra(Intent.EXTRA_SUBJECT , "The title");
+                startActivity(Intent.createChooser(intent1, "share"));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_ACCESS_COARSE_LOCATION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (locationManager != null) {
-                        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: Consider calling
-                            //    Activity#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for Activity#requestPermissions for more details.
-                            return;
-                        }
-                        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        if (location != null) {
-                            double latitude = location.getLatitude();
-                            double longitude = location.getLongitude();
-                            Toast.makeText(getApplication(),"latitude: "+ latitude +" longitude: "+longitude, Toast.LENGTH_LONG).show();
-
-                        }
-                    }
-                    // All good!
-                } else {
-                    boolean isGPSEnabled = locationManager
-                            .isProviderEnabled(LocationManager.GPS_PROVIDER);
-                    if(!isGPSEnabled)
-                    {
-                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-
-                        // Setting Dialog Title
-                        alertDialog.setTitle("GPS settings");
-
-                        // Setting Dialog Message
-                        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
-
-                        // On pressing Settings button
-                        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int which) {
-                                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                                startActivity(intent);
-                            }
-                        });
-
-                        // on pressing cancel button
-                        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                                finish();
-                            }
-                        });
-
-                        // Showing Alert Message
-                        alertDialog.show();
-
-                    }
-
-                }
-
-                break;
-        }
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (googleApiClient != null) {
-            googleApiClient.connect();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        googleApiClient.disconnect();
-        super.onStop();
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-        Log.i(MainActivity.class.getSimpleName(), "Connected to Google Play Services!");
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-
-            double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
-            String url = String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s",
-                    lat, lon, APP_ID);
-            new GetWeatherTask(textView).execute(url);
-        }
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-        Log.i(MainActivity.class.getSimpleName(), "Can't connect to Google Play Services!");
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        viewPager.setCurrentItem(tab.getPosition());
-
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-
-    }
-
-
-    private class GetWeatherTask extends AsyncTask<String, Void, String> {
-        private TextView textView;
-
-        public GetWeatherTask(TextView textView) {
-            this.textView = textView;
-        }
-
         @Override
-        protected String doInBackground(String... strings) {
-            String weather = "UNDEFINED";
-            try {
-                URL url = new URL(strings[0]);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        public void onRequestPermissionsResult( int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults){
+            switch (requestCode) {
+                case PERMISSION_ACCESS_COARSE_LOCATION:
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        if (locationManager != null) {
+                            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                // TODO: Consider calling
+                                //    Activity#requestPermissions
+                                // here to request the missing permissions, and then overriding
+                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                //                                          int[] grantResults)
+                                // to handle the case where the user grants the permission. See the documentation
+                                // for Activity#requestPermissions for more details.
+                                return;
+                            }
+                            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                double latitude = location.getLatitude();
+                                double longitude = location.getLongitude();
+                                Toast.makeText(getApplication(), "latitude: " + latitude + " longitude: " + longitude, Toast.LENGTH_LONG).show();
 
-                InputStream stream = new BufferedInputStream(urlConnection.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
-                StringBuilder builder = new StringBuilder();
+                            }
+                        }
+                        // All good!
+                    } else {
+                        boolean isGPSEnabled = locationManager
+                                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+                        if (!isGPSEnabled) {
+                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
 
-                String inputString;
-                while ((inputString = bufferedReader.readLine()) != null) {
-                    builder.append(inputString);
-                }
+                            // Setting Dialog Title
+                            alertDialog.setTitle("GPS settings");
 
-                JSONObject topLevel = new JSONObject(builder.toString());
-                JSONObject main = topLevel.getJSONObject("main");
-                weather = String.valueOf(main.getDouble("temp"));
+                            // Setting Dialog Message
+                            alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
 
-                urlConnection.disconnect();
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
+                            // On pressing Settings button
+                            alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                    startActivity(intent);
+                                }
+                            });
+
+                            // on pressing cancel button
+                            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                    finish();
+                                }
+                            });
+
+                            // Showing Alert Message
+                            alertDialog.show();
+
+                        }
+
+                    }
+
+                    break;
             }
-            return weather;
+        }
+        @Override
+        protected void onStart () {
+            super.onStart();
+            if (googleApiClient != null) {
+                googleApiClient.connect();
+            }
         }
 
         @Override
-        protected void onPostExecute(String temp) {
-            textView.setText("Current Weather: " + temp);
+        protected void onStop () {
+            googleApiClient.disconnect();
+            super.onStop();
         }
+
+
+        @Override
+        public void onConnected (@Nullable Bundle bundle){
+
+            Log.i(MainActivity.class.getSimpleName(), "Connected to Google Play Services!");
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+                double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
+                String url = String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s",
+                        lat, lon, APP_ID);
+                new GetWeatherTask(textView).execute(url);
+            }
+
+        }
+
+        @Override
+        public void onConnectionSuspended ( int i){
+
+        }
+
+        @Override
+        public void onConnectionFailed (@NonNull ConnectionResult connectionResult){
+
+            Log.i(MainActivity.class.getSimpleName(), "Can't connect to Google Play Services!");
+
+        }
+
+        @Override
+        public void onLocationChanged (Location location){
+
+        }
+
+        @Override
+        public void onTabSelected (TabLayout.Tab tab){
+            viewPager.setCurrentItem(tab.getPosition());
+
+        }
+
+        @Override
+        public void onTabUnselected (TabLayout.Tab tab){
+
+        }
+
+        @Override
+        public void onTabReselected (TabLayout.Tab tab){
+
+        }
+
+
+        private class GetWeatherTask extends AsyncTask<String, Void, String> {
+            private TextView textView;
+
+            public GetWeatherTask(TextView textView) {
+                this.textView = textView;
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+                String weather = "UNDEFINED";
+                try {
+                    URL url = new URL(strings[0]);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream stream = new BufferedInputStream(urlConnection.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuilder builder = new StringBuilder();
+
+                    String inputString;
+                    while ((inputString = bufferedReader.readLine()) != null) {
+                        builder.append(inputString);
+                    }
+
+                    JSONObject topLevel = new JSONObject(builder.toString());
+                    JSONObject main = topLevel.getJSONObject("main");
+                    weather = String.valueOf(main.getDouble("temp"));
+
+                    urlConnection.disconnect();
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+                return weather;
+            }
+
+            @Override
+            protected void onPostExecute(String temp) {
+                textView.setText("Current Weather: " + temp);
+            }
+        }
+
     }
 
-}
 
--- app/src/main/java/com/example/weather/SplashActivity.java
+++ app/src/main/java/com/example/weather/SplashActivity.java
@@ -1,11 +1,11 @@
 package com.example.weather;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
