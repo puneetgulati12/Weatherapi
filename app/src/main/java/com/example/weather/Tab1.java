310df12d596b00d448da998653774fe49c803a0c
puneetgulati12 <puneetgulati97@gmail.com>
2019-07-10 12:26:22 +0530

    changes

--- app/src/main/java/com/example/weather/Tab1.java
+++ app/src/main/java/com/example/weather/Tab1.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -28,57 +30,65 @@ public class Tab1 extends Fragment {
     private static final String APP_ID = "352e84b0ebdd052bca879172b8cf1ba";
     private GoogleApiClient googleApiClient;
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+return;
+        }
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
+        String units = "imperial";
+
+        final String baseurl =  String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=%s&appid=%s",
+                lat, lon,units  , APP_ID);
+
+        OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder()
+                .url(baseurl).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myresponse = response.body().string();
+                Log.d("response" , myresponse);
+
+                String result = myresponse;
+
+                Gson gson = new Gson();
+
+                weatherApi myobj = gson.fromJson(result , weatherApi.class);
+
+
+
+
+            }
+        });
+        super.onViewCreated(view, savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
+
+
         //Returning the layout file after inflating
         //Change R.layout.tab1 in you classes
 
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
 
-            }
-            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-            double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
-            String units = "imperial";
-
-            final String baseurl =  String.format("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=%s&appid=%s",
-                    lat, lon,units  , APP_ID);
-
-            OkHttpClient client = new OkHttpClient();
-
-            final Request request = new Request.Builder()
-                    .url(baseurl).build();
-
-            client.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    call.cancel();
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    final String myresponse = response.body().string();
-                    Log.d("response" , myresponse);
-
-                    String result = myresponse;
-
-                    Gson gson = new Gson();
-
-                    weatherApi myobj = gson.fromJson(result , weatherApi.class);
-
-
-
-
-                }
-            });
 
 
 
